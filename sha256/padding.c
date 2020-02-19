5e7c0c6bb0622df8a35e3bf21e6b15e001c66e8e
markgilmore10 <mrkgilmore@gmail.com>
2020-02-19 14:39:27 +0000

    Padding updated

--- sha256/padding.c
+++ sha256/padding.c
@@ -5,6 +5,13 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+union block {
+
+	uint64_t sixfour[8];
+	uint32_t threetwo[16];
+	uint8_t eight[64];
+};
+
 uint64_t nozerobytes(uint64_t nobits) {
 
 	uint64_t result = 512ULL - (nobits % 512ULL);
@@ -32,10 +39,17 @@ int main(int argc, char *argv[]) {
 
 	uint8_t b;
 	uint64_t nobits;
+	union block M;
+	uint8_t i;
 
-	for (nobits = 0; fread(&b, 1, 1, infile) == 1; nobits += 8) {
+	/*for (nobits = 0; fread(&b, 1, 1, infile) == 1; nobits += 8) {
 
 		printf("%02x" PRIx8 "\n", nobits);
+	}*/
+	
+	for (nobits = 0, i = 0; fread(&M.eight[i], 1, 1, infile) == 1; nobits += 8) {
+
+		printf("%02" PRIx8, M.eight[i]);
 	}
 
 	printf("%02" PRIx8 " ", 0x80); // Bits: 1000 0000
